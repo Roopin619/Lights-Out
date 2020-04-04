da3a1015d76999841ac125fd94a98bb0146e2551
Roopin <bhadriroopin748@gmail.com>
2020-04-04 08:22:11 +0530

    Updates

---
+++ precache-manifest.6d6b41ccbae3ac1afbac62cf55cbb0d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d74c751a62a4df05a9d6",
+    "url": "/Lights-Out/static/css/main.4b250289.chunk.css"
+  },
+  {
+    "revision": "d74c751a62a4df05a9d6",
+    "url": "/Lights-Out/static/js/main.d74c751a.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/Lights-Out/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "bcedacc98465ac2cb9bb",
+    "url": "/Lights-Out/static/js/runtime~main.bcedacc9.js"
+  },
+  {
+    "revision": "ebdd182dbf5ae43274d6c929f033feb9",
+    "url": "/Lights-Out/index.html"
+  }
+];
